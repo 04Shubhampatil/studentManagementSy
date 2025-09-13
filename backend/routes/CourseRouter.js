fca6941fe81d56a2417fe7070e7f687ef1dfec84
04Shubhampatil <04Shubhampatil>
2025-09-13 10:22:44 +0530

    crud
    Signed-off-by: 04Shubhampatil <04Shubhampatil>

--- backend/routes/CourseRouter.js
+++ backend/routes/CourseRouter.js
@@ -1,50 +1,9 @@
-
-import { Router } from "express";
-import course from '../models/CourseModel.js'
+import Router from 'express'
+import { getList,getById,postList } from '../middleware/courseMiddleware.js'
 const router = Router()
 
-router.get("/", (req, res) => {
-    res.json({
-        message: "Hello by course api"
-    })
-})
-
-router.get("/getList", async (req, res) => {
-    try {
-        const allCourse = await course.find({})  //modelMese data ko nikalna
-        res.json(allCourse)
-    } catch (error) {
-        res.json({ message: error })
-    }
-})
-
-router.get("/:id", async (req, res) => {
-    try {
-        const courseid = await course.findById(req.params.id)  //modelMese data ko nikalna
-        res.json(courseid)
-    } catch (error) {
-        res.json({ message: error })
-    }
-})
-
-router.post("/add", async (req, res) => {
-
-    const courseData = await course.create({
-        courseName: req.body.courseName,
-        courseBanner: req.body.courseBanner,
-        courseDiscription: req.body.courseDiscription,
-        teacherId:req.body.teacherId
-    })
-
-    try {
-        const savecourse = await courseData.save();
-        res.json(savecourse)
-
-    } catch (error) {
-        res.json({ message: error })
-
-    }
-})
-
+router.get("/getList",getList)
+router.get("/:id",getById)
+router.post("/postList",postList)
 
 export default router
